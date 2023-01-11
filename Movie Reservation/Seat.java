5f39e2027485da0887904dc055904a82ec9d4873
Gexto <97969977+Gexto@users.noreply.github.com>
2023-01-11 14:54:31 -0500

    Create Seat.java

---
+++ Movie Reservation/Seat.java
@@ -0,0 +1,10 @@
+//Parent Class
+public abstract class Seat{
+  public int adultPrice;
+  public int childPrice;
+  public String comfort;
+  public String seatMaterial;
+
+  public abstract void printInfo();
+  
+}
